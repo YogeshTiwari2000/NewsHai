4024c22313d45aaf23087119e240c68c41ed90b3
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-01-24 11:36:58 +0530

    check

--- src/App.js
+++ src/App.js
@@ -24,22 +24,23 @@ export default class App extends Component {
     return (
       <div>
         <Router>
-        <Navbar/>
-        {/* <LoadingBar
+          <Navbar />
+          {/* <LoadingBar
         color='#f11946'
         progress={this.state.progress}
       /> */}
-        <Routes>
-          <Route path="/" element={<News setProgress={this.setProgress}  key="general"  country="in" category="general" />}></Route>
-          <Route path="/general" element={<News setProgress={this.setProgress}  key="general"  country="in" category="general" />}></Route>
-          <Route path="/technology" element={<News setProgress={this.setProgress}  key="technology"  country="in" category="technology"/>}></Route>
-          <Route path="/business" element={<News setProgress={this.setProgress}  key="business"  country="in" category="business"/>}></Route>
-          <Route path="/sports" element={<News setProgress={this.setProgress}  key="sports" country="in"category="sports"/>}></Route>
-          <Route path="/entertainment" element={<News setProgress={this.setProgress}  key="entertainment" country="in"category="entertainment"/>}></Route>
-          <Route path="/health" element={<News setProgress={this.setProgress}  key="health" country="in"category="health"/>}></Route>
-          <Route path="/science" element={<News setProgress={this.setProgress}  key="science" country="in"category="science"/>}></Route>
-        </Routes>
-        {/* Hello Yogesh Tiwari Sir !!!!!{this.y} */}
+          <Routes>
+            <Route path="/NewsHai" element={<News setProgress={this.setProgress} key="general" country="in" category="general" />}></Route>
+            <Route path="/" element={<News setProgress={this.setProgress} key="general" country="in" category="general" />}></Route>
+            <Route path="/general" element={<News setProgress={this.setProgress} key="general" country="in" category="general" />}></Route>
+            <Route path="/technology" element={<News setProgress={this.setProgress} key="technology" country="in" category="technology" />}></Route>
+            <Route path="/business" element={<News setProgress={this.setProgress} key="business" country="in" category="business" />}></Route>
+            <Route path="/sports" element={<News setProgress={this.setProgress} key="sports" country="in" category="sports" />}></Route>
+            <Route path="/entertainment" element={<News setProgress={this.setProgress} key="entertainment" country="in" category="entertainment" />}></Route>
+            <Route path="/health" element={<News setProgress={this.setProgress} key="health" country="in" category="health" />}></Route>
+            <Route path="/science" element={<News setProgress={this.setProgress} key="science" country="in" category="science" />}></Route>
+          </Routes>
+          {/* Hello Yogesh Tiwari Sir !!!!!{this.y} */}
         </Router>
       </div>
     )
